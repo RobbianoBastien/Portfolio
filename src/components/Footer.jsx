3999af9a9a1d1507862330cf08f1ad3ac531d50f
robbianobastien <bastien.robbiano@live.fr>
2025-10-20 14:34:20 +0200

    linkind link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,6 @@ import linkedin from '../assets/images/linkedin.webp'
 function Footer(){
     return(
     <div className="footer">
-        <p className='footer__texte'>Robbiano Bastien</p>
-        <p className='footer__texte'>bastien.robbiano@live.fr</p>
         <a 
             href="https://github.com/RobbianoBastien" 
             target="_blank" 
@@ -15,7 +13,7 @@ function Footer(){
             <img src={github} alt="logo github" className="footer__img"/>
         </a>
         <a 
-            href="www.linkedin.com/in/bastien-robbiano" 
+            href="https://www.linkedin.com/in//bastien-robbiano" 
             target="_blank" 
             aria-label="Visitez mon profil LinkedIn"
         >
